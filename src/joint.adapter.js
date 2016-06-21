274b22ad491cc02e72a1660d444daa6e20576dfb
magnus <magnus.henning@gmail.com>
2016-06-21 16:47:19 +0200

    joint adapter : Add support for CommonJS, AMD, plain js

--- src/joint.adapter.js
+++ src/joint.adapter.js
@@ -1,58 +1,80 @@
-(function (_window) {
-  if (!_window.JointAdapter) {
+(function (root, factory) {
+  var module = 'joint-adapter';
 
-    function preload(dependencies, callback) {
-      dependencies = dependencies || [];
+  // AMD
+  if(typeof define === "function" && define.amd) {
+    define(module, [], function(){
+      return (root[module] = factory(root));
+    });
 
-      var preloadList = this.plugins.preload;
-
-      if (preloadList.length) preloadList.forEach(function (plugin) {
-        plugin(dependencies);
-      });
-
-      if (callback) callback(dependencies);
-    }
-
-    function load(joint, callback) {
-
-      var loadList = this.plugins.load;
-
-      if (loadList.length) loadList.forEach(function (plugin) {
-        plugin(joint);
-      });
-
-      if (callback) callback(joint);
-    }
-
-    function JointAdapter() {
-      var _this = this;
-
-      function addPlugin(plugin, list) {
-        if (list.indexOf(plugin) === -1) list.push(plugin);
-      }
-
-      function addPreloadPlugin(plugin) {
-        addPlugin(plugin, _this.plugins.preload);
-      }
-
-      function addLoadPlugin(plugin) {
-        addPlugin(plugin, _this.plugins.load);
-      }
-
-      _this.plugins = {
-        preload:  [],
-        load:     []
-      };
-
-      _this.actions = {
-        addPreloadPlugin: addPreloadPlugin,
-        addPlugin:        addLoadPlugin
-      };
-    }
-
-    JointAdapter.prototype.preload  = preload;
-    JointAdapter.prototype.load     = load;
-
-    _window.JointAdapter = new JointAdapter();
   }
-})(this);
+
+  // CommonJS + NodeJS
+  else if(typeof module === "object" && module.exports) {
+    module.exports = (root[module] = factory(root));
+  }
+
+  // Plain JS
+  else root[module] = factory(root);
+
+}(this, function (root) {
+
+  function preload(dependencies, callback) {
+    dependencies = dependencies || [];
+
+    var preloadList = this.plugins.preload;
+
+    if (preloadList.length) preloadList.forEach(function (plugin) {
+      plugin(dependencies);
+    });
+
+    if (callback) callback(dependencies);
+  }
+
+  function load(joint, callback) {
+
+    var loadList = this.plugins.load;
+
+    if (loadList.length) loadList.forEach(function (plugin) {
+      plugin(joint);
+    });
+
+    if (callback) callback(joint);
+  }
+
+  function JointAdapter() {
+    var _this = this;
+
+    function addPlugin(plugin, list) {
+      if (list.indexOf(plugin) === -1) list.push(plugin);
+    }
+
+    function addPreloadPlugin(plugin) {
+      addPlugin(plugin, _this.plugins.preload);
+    }
+
+    function addLoadPlugin(plugin) {
+      addPlugin(plugin, _this.plugins.load);
+    }
+
+    _this.plugins = {
+      preload:  [],
+      load:     []
+    };
+
+    _this.actions = {
+      addPreloadPlugin: addPreloadPlugin,
+      addPlugin:        addLoadPlugin
+    };
+  }
+
+  JointAdapter.prototype.preload  = preload;
+  JointAdapter.prototype.load     = load;
+
+  var adapter = new JointAdapter();
+
+  if (!root.joint) root.joint = {};
+
+  return root.joint.adapter = adapter;
+
+}));
